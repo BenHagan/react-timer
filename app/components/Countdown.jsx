ce9c551405e8004cfd4c6d54ed5329fea99abc00
Ben Hagan <benhagan@users.noreply.github.com>
2016-11-20 23:48:58 -0600

    Add Navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+  render: () => <div>Countdown.jsx</div>
+});
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: () => <div>Timer.jsx</div>
+});
+
+module.exports = Timer;
