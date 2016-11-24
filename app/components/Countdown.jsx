4b7cfc91c4d7b66bab4df3c7d8650657961d4ac6
Ben Hagan <benhagan@users.noreply.github.com>
2016-11-24 08:38:44 -0600

    Finalize test and add page title

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -25,6 +25,10 @@ var Countdown = React.createClass({
       }
     }
   },
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
   startTimer: function () {
     this.timer = setInterval (() => {
       var newCount = this.state.count - 1;
@@ -32,6 +36,9 @@ var Countdown = React.createClass({
         count: newCount >= 0 ? newCount : 0
       });
 
+      if (newCount === 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
     }, 1000);
   },
   handleSetCountdown: function (seconds) {
@@ -57,6 +64,7 @@ var Countdown = React.createClass({
 
     return (
     <div>
+      <h1 className="page-title">Countdown App</h1>
       <Clock totalSeconds={count} />
       {renderControlArea()}
     </div>
